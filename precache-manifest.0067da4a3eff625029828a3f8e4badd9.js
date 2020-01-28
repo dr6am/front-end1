cef69308e8d45f32b9f7c508946f12c32a8805d8
dr6am <alexanderbasoc@gmail.com>
2020-01-29 00:18:04 +0300

    first commit

---
+++ precache-manifest.0067da4a3eff625029828a3f8e4badd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e431ba30223cdee529a7711155729b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed6d5197c6950e57b95b",
+    "url": "/static/css/main.830e30b0.chunk.css"
+  },
+  {
+    "revision": "7ab8174cbc8e9d723aae",
+    "url": "/static/js/2.d8a256f8.chunk.js"
+  },
+  {
+    "revision": "9b499988666cc8d7d52d70a8c3b2f162",
+    "url": "/static/js/2.d8a256f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed6d5197c6950e57b95b",
+    "url": "/static/js/main.9ef9f47c.chunk.js"
+  },
+  {
+    "revision": "cb722c0f83d9ad2d04d3",
+    "url": "/static/js/runtime-main.bea30945.js"
+  }
+]);
